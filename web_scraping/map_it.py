8c291cd78eb30f83a2bd8e5258141c0dea4a7278
Kemal Diri <kemaldiri@gmail.com>
2020-07-23 23:33:10 +0200

    add map it.

---
+++ web_scraping/map_it.py
@@ -0,0 +1,26 @@
+import webbrowser
+from pandas.io.clipboard import clipboard_get
+from typing import Any
+
+
+def get_content():
+    address = input(
+        "Please paste the address or please enter and what it has copied to clipboard will be used: \n"
+    )
+    if not address:
+        address = clipboard_get()
+    return address
+
+
+def open_browser(address: str = Any):
+    google_maps = "https://www.google.com/maps/place/"
+    webbrowser.open(google_maps + address)
+
+
+def process():
+    address = get_content()
+    open_browser(address)
+
+
+if __name__ == "__main__":
+    process()
