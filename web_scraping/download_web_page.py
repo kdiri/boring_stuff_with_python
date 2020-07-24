4ba169bff04a012b9824691a2c6779e05645b869
Kemal Diri <kemaldiri@gmail.com>
2020-07-24 08:27:55 +0200

    Beautify logging.

--- web_scraping/download_web_page.py
+++ web_scraping/download_web_page.py
@@ -2,17 +2,22 @@
 .. module:: web_scraping.download_web_page
    :synopsis: download web pages using requests module
 """
+import sys
+
 import requests
 from loguru import logger
+
+logger.add(sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>")
 
 
 def get_existing_page():
     page = requests.get("https://automatetheboringstuff.com/files/rj.txt")
     assert page.status_code == requests.codes.ok
-    logger.info(page.text[:250])
+    logger.success(page.text[:250])
     return page
 
 
+@logger.catch
 def get_404():
     page = requests.get("https://inventwithpython.com/page_that_does_not_exist")
     page.raise_for_status()
@@ -38,3 +43,4 @@ def save_page():
 if __name__ == "__main__":
     save_page()
     get_safe_404()
+    get_404()
