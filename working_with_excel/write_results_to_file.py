b50da047395b33fa47d806b5cfe3b31787008baf
Kemal Diri <kemaldiri@gmail.com>
2020-07-27 09:43:09 +0200

    Write results to .py file.

--- working_with_excel/write_results_to_file.py
+++ working_with_excel/write_results_to_file.py
@@ -4,6 +4,9 @@
 """
 
 from openpyxl import load_workbook
+import pprint
+from loguru import logger
+from typing import Dict
 
 
 def load_census_file():
@@ -21,11 +24,20 @@ def compute_population_of_each_country(sheet: load_workbook):
                 new_dict[state.value].update({county.value: {"pop": pop}})
         else:
             new_dict[state.value] = {county.value: {"pop": pop}}
+    return new_dict
+
+
+def write_results_to_file(result_dict: Dict):
+    logger.info(f"| ----- Results will be written into file.")
+    with open("census2010.py", "w") as res_file:
+        res_file.write(f"'Results': {pprint.pformat(result_dict)}")
+    logger.success(f"| ----- Results are in the census2010.py file.")
 
 
 def handle():
-    sheet = load_workbook()
-    compute_population_of_each_country(sheet)
+    sheet = load_census_file()
+    results = compute_population_of_each_country(sheet)
+    write_results_to_file(results)
 
 
 if __name__ == "__main__":
